6c93ed4f8c6d1adc0155a39cceee82977acde775
TeamDman <TeamDman9201@gmail.com>
2025-03-01 02:26:02 -0500

    score card printing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use poche::rules::rule::Rule;
 use poche::state::State;
 
 fn main() -> eyre::Result<()> {
@@ -5,13 +6,46 @@ fn main() -> eyre::Result<()> {
     let mut state = State::new_with_random_players(4);
     println!("{state}");
     println!("Begin game loop");
+    let mut player_score_timeline: Vec<Vec<u32>> = Vec::new();
+    let mut hand_size_timeline: Vec<u32> = Vec::new();
     loop {
-        let action_taken = state.tick()?;
-        if action_taken.is_none() {
-            break;
+        match state.tick()? {
+            None => break,
+            Some(Rule::RoundOver) => {
+                hand_size_timeline.push(state.round.hand_size);
+            }
+            Some(Rule::NextRound) => {
+                player_score_timeline
+                    .push(state.players.iter().map(|player| player.score).collect());
+            }
+            _ => {}
         }
     }
     println!("Game loop ended");
     println!("{state}");
+    println!();
+    println!("Scorecard:");
+    // print player names as table
+    print!("{:<15}", "Hand Size");
+    for player in state.players.iter() {
+        print!("{:<15}", player.id.to_string());
+    }
+    println!();
+    // print player scores as table
+    for (i, (scores, hand_size)) in player_score_timeline.iter().zip(hand_size_timeline.iter()).enumerate() {
+        print!("{:<15}", hand_size);
+        for (j, score) in scores.iter().enumerate() {
+            let previous_score = if i == 0 { 0 } else { player_score_timeline[i - 1][j] };
+            let score_change = score - previous_score;
+            print!("{:<15}", score_change);
+        }
+        println!();
+    }
+    // print final scores
+    println!("{}", "=".repeat(15*(state.players.len()+1)));
+    print!("{:<15}", "Final Score");
+    for player in state.players.iter() {
+        print!("{:<15}", player.score);
+    }
     Ok(())
 }
